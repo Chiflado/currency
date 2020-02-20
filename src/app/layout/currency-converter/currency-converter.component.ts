99510abe4e57ee491d8011c90e7cd957bd068916
Chiflado <prevarabalazs@gmail.com>
2020-02-20 22:19:41 +0100

    Converts amounts after adds inputs

--- src/app/layout/currency-converter/currency-converter.component.ts
+++ src/app/layout/currency-converter/currency-converter.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { CurrencyConverterService } from 'src/app/shared/domain/currency-converter.service';
 import { SumTableData } from 'src/app/shared/data';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-currency-converter',
@@ -19,14 +20,23 @@ export class CurrencyConverterComponent {
   showTable = false;
 
   constructor(private service: CurrencyConverterService) {
+    const amountDebounce = this.amountControl.valueChanges.pipe(
+      debounceTime(1000),
+      distinctUntilChanged()
+    );
+    this.currencyControl.valueChanges.subscribe(() => this.getCurrencies());
+    amountDebounce.subscribe(() => {
+      this.getCurrencies();
+    });
   }
 
   getCurrencies(): void {
     this.showTable = false;
-    this.converted = null;
     this.amount = this.amountControl.value;
     this.currency = this.currencyControl.value;
-    this.service.getExchangeRates(this.currency).subscribe(resp => this.convertAmount(resp.rates.USD));
+    if (this.amount && this.currency) {
+      this.service.getExchangeRates(this.currency).subscribe(resp => this.convertAmount(resp.rates.USD));
+    }
   }
 
   convertAmount(exchRate): void {
@@ -35,17 +45,19 @@ export class CurrencyConverterComponent {
       this.amount = this.setCharAt(this.amount, colonInd, '.');
     }
     this.converted = Number(this.amount) * exchRate;
-    this.addExchangedToTable();
   }
 
   addExchangedToTable() {
+    this.showTable = false;
     const element: SumTableData = {
       amount: this.amount,
       converted: this.converted,
       currency: this.currency
     };
     this.exchangedHistory.push(element);
-    this.showTable = true;
+    setTimeout(() => {
+      this.showTable = true;
+    }, 100);
   }
 
   setCharAt(str: string, index: number, chr: string): string {
