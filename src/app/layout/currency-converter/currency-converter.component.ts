cecfdcb17ab2ef59d7b60c480daeadc92b8f136e
Chiflado <prevarabalazs@gmail.com>
2020-02-20 19:56:24 +0100

    Exchanges currency

--- src/app/layout/currency-converter/currency-converter.component.ts
+++ src/app/layout/currency-converter/currency-converter.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { CurrencyConverterService } from 'src/app/shared/domain/currency-converter.service';
+import { ECANCELED } from 'constants';
 
 @Component({
   selector: 'app-currency-converter',
@@ -10,15 +12,34 @@ export class CurrencyConverterComponent implements OnInit {
 
   converted: number;
   amount;
+  currency: string;
 
   amountControl = new FormControl('');
   currencyControl = new FormControl('');
 
-  constructor() {}
+  constructor(private service: CurrencyConverterService) {
+  }
 
   ngOnInit() {
-    this.converted = 100000;
-    this.amount = 15161516;
+  }
+
+  getCurrencies() {
+    this.converted = null;
+    this.amount = this.amountControl.value;
+    this.currency = this.currencyControl.value;
+    this.service.getExchangeRates(this.currency).subscribe(resp => this.convertAmount(resp.rates.USD));
+  }
+
+  convertAmount(exchRate) {
+    if (this.amount.includes(',')) {
+      const colonInd = this.amount.indexOf(',');
+      this.amount = this.setCharAt(this.amount, colonInd, '.');
+    }
+    this.converted = Number(this.amount) * exchRate;
+  }
+
+  setCharAt(str, index, chr) {
+    return str.substr(0, index) + chr + str.substr(index + 1);
   }
 
 }
